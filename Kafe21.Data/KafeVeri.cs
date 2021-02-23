9ec33bb7724aaa60d4fce835d085ae746a277994
ardasar94 <sarihanarda@gmail.com>
2021-02-23 18:12:06 +0300

    Kafe21.Data EF6 Entegrasyonu

--- Kafe21.Data/KafeVeri.cs
+++ Kafe21.Data/KafeVeri.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Kafe21.Data
 {
-    public class KafeVeri
+    public class KafeVeri : DbContext
     {
-        public int MasaAdet { get; set; } = 20;
-        public List<Urun> Urunler { get; set; } = new List<Urun>();
-        public List<Siparis> AktifSiparisler { get; set; } = new List<Siparis>();
-        public List<Siparis> GecmisSiparisler { get; set; } = new List<Siparis>();
+        public KafeVeri() : base("name=KafeVeri")
+        {
 
+        }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SiparisDetay>()
+                .HasRequired(x => x.Urun)
+                .WithMany(x => x.SiparisDetay)
+                .HasForeignKey(x => x.UrunId)
+                .WillCascadeOnDelete(false);
+        }
+        public int MasaAdet { get; set; } = 20;
+        public DbSet<Urun> Urunler { get; set; }
+        public DbSet<Siparis> Siparisler { get; set; }
+        public DbSet<SiparisDetay> SiparisDetaylar { get; set; }
     }
 }
